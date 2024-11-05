a9ceca6e4b4439e3b8c8bbc707ca24e898f4b877
davideoreglio <doreglio@red.software.systems>
2024-11-05 17:01:00 +0100

    add header and footer

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    imports: [],
+    template: ` <p>header works!</p> `,
+    styles: ``,
+})
+export class HeaderComponent {}
