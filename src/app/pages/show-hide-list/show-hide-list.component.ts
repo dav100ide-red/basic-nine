765a0d0b141c52604a7a7e83ac43612ad2a770e3
davideoreglio <doreglio@red.software.systems>
2024-11-06 11:13:36 +0100

    add show-hide-lists files

---
+++ src/app/pages/show-hide-list/show-hide-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'show-hide-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-hide-list.component.html',
+  styleUrl: './show-hide-list.component.scss'
+})
+export class ShowHideListComponent {
+
+}
